e2b0629af27638e059687af1b78c641e3f3d3d88
marcinprusko <marcinprusko@gmail.com>
2017-02-27 22:37:57 +0100

    warunki

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+﻿// plik scripts.js
+
+var a = prompt('wpisz wartość a');
+
+var b = prompt('wpisz wartość b');
+
+var value = (a*a) + (2 * a * b) - (b*b);
+console.log(value);
+
+if ( value > 0 ) {
+	console.log('wynik dodatni');
+} else if ( value < 0 ) {
+	console.log('wynik ujemny');
+}
+
+
+switch(value) {
+	case 0:
+		console.log('wynik jest równy zero');
+}
